b39e531829d93709c2ec6a30c05b3c323601029d
edG5LATAN <136633393+edG5LATAN@users.noreply.github.com>
2024-11-04 21:40:32 -0600

    Update Services.jsx

--- src/services/Services.jsx
+++ src/services/Services.jsx
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const api=process.env.API_KEY || "https://www.tutiempo.net/"
+const api=process.env.API_KEY
 
 export const traerData=(latitud,longitud,setdata)=>{
   axios.get(`https://api.tutiempo.net/json/?lan=es&apid=${api}&ll=${latitud},${longitud}`)
