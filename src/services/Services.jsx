ae1c51e43fe19cc718f0f0742f8badc28a2472ce
edG5LATAN <136633393+edG5LATAN@users.noreply.github.com>
2024-11-30 00:14:14 -0600

    Update Services.jsx
    se cambio variablede entorno de service

--- src/services/Services.jsx
+++ src/services/Services.jsx
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const api=process.env.API_KEY || "4wT44aXaXqXNZ7z";
+const api=process.env.REACT_APP_API;
 
 export const traerData=(latitud,longitud,setdata)=>{
   axios.get(`https://api.tutiempo.net/json/?lan=es&apid=${api}&ll=${latitud},${longitud}`)
